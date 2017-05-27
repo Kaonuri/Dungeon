b03c6417a3c2c9760fbed6d4845ae51ec2f57acb
Kaonuri <hon454@naver.com>
2017-05-27 16:31:03 +0900

    임시 이동 구현

---
+++ Assets/Scripts/TouchPadMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TouchPadMovement : MonoBehaviour
+{
+    private AirVRStereoCameraRig cameraRig;
+
+    public float speed;
+
+    private void Awake()
+    {
+        cameraRig = GetComponentInChildren<AirVRStereoCameraRig>();        
+    }
+
+    private void Update()
+    {
+        if (cameraRig.isBoundToClient)
+        {
+            float axisX = AirVRInput.GetAxis(cameraRig, AirVRInput.Touchpad.Axis.DragX);
+            float axisY = AirVRInput.GetAxis(cameraRig, AirVRInput.Touchpad.Axis.DragY);
+
+            if (Mathf.Abs(axisX + axisY) > 0f)
+            {
+                Vector3 direction = new Vector3(axisY, 0f, -axisX);
+                transform.position += direction * speed * Time.deltaTime;
+            }          
+        }
+    }
+}
