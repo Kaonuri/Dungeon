f8976cad8bc940ea3f460f36809f25b0334b9c22
Kaonuri <hon454@naver.com>
2017-05-27 17:45:21 +0900

    트래킹 기본 설정 완료

---
+++ Assets/Scripts/UpdatePosition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdatePosition : MonoBehaviour
+{
+    [SerializeField] private Transform trackerTransform;
+    [SerializeField] private Vector3 offset;
+
+	private void Update ()
+	{
+	    transform.position = trackerTransform.position + offset;
+	}
+}
---
+++ Assets/Scripts/UpdateRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateRotation : MonoBehaviour
+{
+    [SerializeField] private Transform trackerTransform;
+
+    private void Update()
+    {
+        transform.rotation = trackerTransform.rotation;
+    }
+}
