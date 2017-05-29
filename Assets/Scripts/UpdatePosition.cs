b9815d2b6ec50be34e3475add01a49f97aeb7874
Kaonuri <hon454@naver.com>
2017-05-30 05:54:13 +0900

    머리 위치 보간 작업 전 커밋

--- Assets/Scripts/UpdatePosition.cs
+++ Assets/Scripts/UpdatePosition.cs
@@ -1,14 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class UpdatePosition : MonoBehaviour
 {
     [SerializeField] private Transform trackerTransform;
     [SerializeField] private Vector3 offset;
 
-	private void Update ()
+	private void LateUpdate ()
 	{
+	    Vector3 back = trackerTransform.forward * -1f;
+	    Vector3 down = trackerTransform.up * -1f;
+
+//        transform.position = trackerTransform.transform.position - trackerTransform.forward * offset.z;	    
+//        print(Vector3.Distance(transform.position, trackerTransform.position));	  
+
 	    transform.position = trackerTransform.position + offset;
 	}
 }
