70e61e9aabd820e632fd7a77520a59df80469e7f
Kaonuri <hon454@naver.com>
2017-05-30 06:32:17 +0900

    출구 인터렉션 및 메시지 작성

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    public GameObject message;    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        message.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        message.SetActive(false);
+    }
+}
