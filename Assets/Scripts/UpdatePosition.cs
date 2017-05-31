620a7245896e0245424fb0b3e187e8d62d392a63
Kaonuri <hon454@naver.com>
2017-05-31 11:41:31 +0900

    최종 빌드

--- Assets/Scripts/UpdatePosition.cs
+++ Assets/Scripts/UpdatePosition.cs
@@ -8,8 +8,12 @@ public class UpdatePosition : MonoBehaviour
     private void LateUpdate()
     {
         float offsetMagnitude = Vector3.Magnitude(offset);
-        Vector3 direction = (trackerTransform.transform.forward + trackerTransform.up).normalized;
 
-        transform.position = trackerTransform.position + direction * offsetMagnitude;
+//        Vector3 offsetPosition = trackerTransform.TransformPoint(offset);
+
+        Vector3 direction = (trackerTransform.transform.forward + trackerTransform.up).normalized * -1;
+        transform.position = trackerTransform.position + trackerTransform.localToWorldMatrix.MultiplyVector(offset); //direction * offsetMagnitude);
+
+//        transform.position = offsetPosition;
     }
 }
